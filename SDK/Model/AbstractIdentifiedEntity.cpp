924523daa9d8acaa5fb8e18e43df1d2e380fe4a8
Vinokurov Kirill <vinokurov@elewise.com>
2018-07-27 11:29:05 +0400

    feature/EB-1

--- SDK/Model/AbstractIdentifiedEntity.cpp
+++ SDK/Model/AbstractIdentifiedEntity.cpp
@@ -1,8 +1,10 @@
 #include "AbstractIdentifiedEntity.h"
 #include <SDK/Helpers/EntityHelper.h>
+#include <SDK/Exceptions/NotFoundException.h>
 
-
-AbstractIdentifiedEntity::AbstractIdentifiedEntity()
+AbstractIdentifiedEntity::AbstractIdentifiedEntity() :
+    AbstractEntity(),
+    _valid(false)
 {
 
 }
@@ -15,7 +17,29 @@ QUuid AbstractIdentifiedEntity::getId() const
 void AbstractIdentifiedEntity::getById(const QUuid &id, const AbstractDatabaseProvider *provider)
 {
     auto result = provider->exec(QString("select * from \"Enerbase\".\"" + QString(getTableName()) + "\" where \"Id\"='" + id.toString() + "' limit 1;"));
+    if (result->isEmpty())
+    {
+        throw NotFoundException();
+    }
     EntityHelper::Instance().Load(result.get(), this);
+    setIsValid(true);
+    setIsNew(false);
+}
+
+void AbstractIdentifiedEntity::save(const AbstractDatabaseProvider *provider)
+{
+    if (isNew())
+    {
+        QStringList fields;
+        EntityHelper::Instance().GetFields(this, fields, '"');
+
+        QString sql("insert into \"Enerbase\".\"" + QString(getTableName()) + "\" (" + fields.join(',') + ") values ();");
+        qDebug() << sql;
+    }
+    else
+    {
+
+    }
 }
 
 void AbstractIdentifiedEntity::setId(QUuid Id)
@@ -27,4 +51,19 @@ void AbstractIdentifiedEntity::setId(QUuid Id)
 
     m_Id = Id;
     emit IdChanged(m_Id);
+}
+
+bool AbstractIdentifiedEntity::isValid() const
+{
+    return _valid;
+}
+
+void AbstractIdentifiedEntity::setIsValid(bool value)
+{
+    if (_valid == value)
+    {
+        return;
+    }
+    _valid = value;
+    emit ValidChanged(value);
 }
--- SDK/Model/AbstractIdentifiedEntity.h
+++ SDK/Model/AbstractIdentifiedEntity.h
@@ -10,7 +10,7 @@ class SDKSHARED_EXPORT AbstractIdentifiedEntity : public AbstractEntity
 {
     Q_OBJECT
 
-    Q_PROPERTY(QUuid Id READ getId WRITE setId NOTIFY IdChanged)
+    Q_PROPERTY(QUuid Id READ getId WRITE setId NOTIFY IdChanged STORED true)
 
     QUuid m_Id;
 
@@ -20,12 +20,24 @@ public:
     QUuid getId() const;
 
     virtual void getById(const QUuid &id, const AbstractDatabaseProvider *provider);
+    virtual void save(const AbstractDatabaseProvider *provider);
 
 public slots:
     void setId(QUuid Id);
 
 signals:
     void IdChanged(QUuid Id);
+    void ValidChanged(bool valid);
+
+    // AbstractEntity interface
+public:
+    virtual bool isValid() const override;
+
+private:
+    bool _valid;
+
+protected:
+    void setIsValid(bool value);
 };
 
 #endif // ABSTRACTIDENTIFIEDENTITY_H
